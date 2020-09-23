836e6690f748d909052b9a3036271c808833fa4f
Luís Henriques <luis.henriques@uniplaces.com>
2020-09-23 18:02:01 +0100

    Change site metadata (#3)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `santos-luis.github.io`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Luis Santos`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    description: `Personal tech blog`,
+    siteUrl: `https://santos-luis.github.io`,
   },
   plugins: [
     {
