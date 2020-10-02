a02f7a37ae8c274a0e218dd89d4fa46ddb94456e
Santos-luis <luissh5@outlook.com>
2020-10-02 15:04:43 +0100

    Add theme images and separate header on a different component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,33 +1,11 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import { ThemeToggler } from 'gatsby-plugin-dark-mode';
-import SantosLuisLogo from '@assets/santos-luis-logo.svg';
+import Header from '@components/header';
 import styles from './layout.module.scss';
 
 const Layout = ({ location, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`;
   const isRootPath = location.pathname === rootPath;
   let mainClassName, footerClassName;
-
-  const header = (
-      <React.Fragment>
-        <Link className={styles.headerLinkHome} to="/">
-          <SantosLuisLogo className={styles.headerLogo} />
-        </Link>
-        <ThemeToggler>
-          {({ theme, toggleTheme }) => (
-            <label>
-              <input
-                type="checkbox"
-                onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
-                checked={theme === 'dark'}
-              />{' '}
-              Dark mode
-            </label>
-          )}
-        </ThemeToggler>
-      </React.Fragment>
-  );
 
   if (!isRootPath) {
     mainClassName = styles.nonRootMainClassName;
@@ -36,7 +14,9 @@ const Layout = ({ location, children }) => {
 
   return (
     <div className={styles.globalWrapper} data-is-root-path={isRootPath}>
-      <header className={styles.globalHeader}>{header}</header>
+      <header className={styles.globalHeader}>
+        <Header />
+      </header>
       <main className={mainClassName}>{children}</main>
       <footer className={footerClassName}>
         {new Date().getFullYear()}
